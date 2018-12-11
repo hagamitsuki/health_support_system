968c061526d8b184fbf4033ed347e8350f2ffc99
mitsuki.haga <hagamitsu4928@gmail.com>
2018-12-11 23:15:12 +0900

    Add Mood new

---
+++ src/controllers/moods/MoodsNewServlet.java
@@ -0,0 +1,44 @@
+package controllers.moods;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Mood;
+
+/**
+ * Servlet implementation class ReportsNewServlet
+ */
+@WebServlet("/moods/new")
+public class MoodsNewServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MoodsNewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("_token", request.getSession().getId());
+
+        Mood m = new Mood();
+        m.setMood_date(new Date(System.currentTimeMillis()));
+        request.setAttribute("mood", m);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/moods/new.jsp");
+        rd.forward(request, response);
+    }
+
+}
